c4dac7f09e4bfd3257a133ecc4c5f2dfcaf2d96c
mmaneh <mmanemkhitaryan@gmail.com>
2025-09-29 19:51:26 +0400

    non stable counting sort

---
+++ counting_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void countingSort(std::vector<int>& arr) {
+    if (arr.empty()) {
+        return;
+    }
+
+    int min = *std::min_element(arr.begin(), arr.end());
+    int max = *std::max_element(arr.begin(), arr.end());
+
+
+    int size = max - min + 1;
+    std::vector<int> count(size, 0);
+
+    for (int num : arr) {
+        ++count[num - min];
+    }
+
+
+    
+    int j = 0;
+    for (int i = 0; i < size; ++i) {
+        while (count[i]--) {
+            arr[j++] = i + min;
+        
+        }
+    }
+
+
+}
+
+
+
+int main() {
+    std::vector<int> arr = {4, 2, -1, 2, 0, 5, -3, 4};
+    countingSort(arr);
+
+    
+    for (int a : arr) {
+        std::cout << a << " ";
+    }
+
+
+    return 0;
+}

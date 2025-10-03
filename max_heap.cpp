eba8583ded45be6907a179ec88efa42cce1145b7
mmaneh <mmanemkhitaryan@gmail.com>
2025-10-04 00:19:02 +0400

    binary heap

---
+++ max_heap.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <iostream>
+class max_heap {
+    private:
+        std::vector<int> heap;
+        int parent(int i) {return (i - 1) / 2;}
+        int left(int i) {return 2 * i + 1;}
+        int right(int i) {return 2 * i + 2;}
+
+        void heapify(int i) {
+            int l = left(i);
+            int r = right(i);
+            int idx = i;
+
+            if(l < heap.size() && heap[idx] < heap[l]) {
+                idx = l;
+            }
+            if(r < heap.size() && heap[idx] < heap[r]) {
+                idx = r;
+            }
+            if (idx != i) {
+                std::swap(heap[idx], heap[i]);
+                heapify(idx);
+            }
+        }
+    public:
+        max_heap() = default;
+        ~max_heap() = default;
+
+        int top() {
+            return heap[0];
+        }
+
+
+        void push(int val) {
+            heap.push_back(val);
+	    int i = heap.size() - 1;
+	    int  p = parent(i);
+            while(i > 0 && heap[p] < heap[i]) {
+		std::swap(heap[p], heap[i]);
+		i = parent(i);
+		p = parent(i);
+	    }
+        }
+
+        void pop() {
+            std::swap(heap[0], heap[heap.size() - 1]);
+            heap.pop_back();
+            heapify(0);
+        }
+	void print() {
+	    for (int h : heap) std::cout << h << " ";
+        }	    
+};
+int main() {
+    max_heap hp;
+    hp.push(5);
+    hp.push(15);
+    hp.push(30);
+    hp.push(20);
+
+    hp.print();
+}
